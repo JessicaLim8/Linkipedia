b4ebfdec47f67777e39644058db2185a579087fd
JessicaLim8 <jessicalim@bell.net>
2020-03-23 18:23:32 -0400

    add outline for parser

---
+++ src/DataParser.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.util.ArrayList;
+
+
+public class DataParser {
+	
+	public static ArrayList<Node> parseNodes(File nodes) {
+		
+	}
+	
+	public static ArrayList<Pair<Integer, ArrayList<Integer>>> parseConnections(File connections) {
+		
+	}
+	
+	public static ArrayList<Node> parseCategories(File connections) {
+		
+	}
+	
+	public static ArrayList<Node> parseNodes(File connections) {
+		
+	}
+	
+	
+	
+}
