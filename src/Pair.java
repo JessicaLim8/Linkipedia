f5aad705bb3d3af6810e26fe4b24965d9224707f
JessicaLim8 <jessicalim@bell.net>
2020-03-24 23:08:07 -0400

    add tuple class

---
+++ src/Pair.java
@@ -0,0 +1,29 @@
+public class Pair<A, B> {
+    private A a;
+    private B b;
+
+    public Pair(A a, B b) {
+        this.setA(a);
+        this.setB(b);
+    }
+
+    public A getA() {
+        return a;
+    }
+
+    public void setA(A a) {
+        this.a = a;
+    }
+
+    public B getB() {
+        return b;
+    }
+
+    public void setB(B b) {
+        this.b = b;
+    }
+    
+    public boolean equals(Pair<A, B> that) {
+    	return (this.a == that.getA() && this.b == that.getB());
+    }
+}
