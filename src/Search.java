89f5ee9c786dc82f4b188a64a0889f84e520e39b
PranayKotian <lohitkotian@yahoo.ca>
2020-03-25 23:34:58 -0400

    minor search module comments

--- src/Search.java
+++ src/Search.java
@@ -4,6 +4,11 @@ LEXIGRAPHICAL SEARCH
 INPUT
 ArrayList of type T
 SearchTerm of type T
+Comparator
+Integer n (how many terms)
+
+RETURNS
+position of hit (-1 if not found)
 */
 
 public class Search { 
