2e95b0dcdc2e2f69bfbfa97dc394dbcd82361a10
PranayKotian <lohitkotian@yahoo.ca>
2020-03-28 03:18:55 -0400

    first two binary search functions completed

--- src/Search.java
+++ src/Search.java
@@ -15,16 +15,48 @@ import java.util.Comparator;
 import java.util.ArrayList;
 
 public class Search { 
-    public static int binarySearch (Comparable [] arr, Comparable target) {
 
+    //Boilerplate binary search code with compareTo
+    public static int binarySearch (Comparable [] arr, Comparable target) {
+        int lo = 0;
+        int hi = arr.length - 1;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if      (target.compareTo(arr[mid]) < 0)
+                hi = mid - 1;
+            else if (target.compareTo(arr[mid]) > 0) 
+                lo = mid + 1;
+            else return mid;
+        }
+
+        //If target not founds, returns -1
+        return -1;
     }
 
-    public static <T> int binarySearchAll (T[] arr, T target, Comparator<T> c) {
 
+    public static <T> int binarySearchAll (T[] arr, T target, Comparator<T> c) {
+        int lo = 0;
+        int hi = arr.length - 1;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if      (c.compareTo(target, arr[mid]) < 0)
+                hi = mid - 1;
+            else if (c.compareTo(target, arr[mid]) > 0) 
+                lo = mid + 1;
+            else return mid;
+        }
+
+        //If target not founds, returns -1
+        return -1;
     }
 
     //added <T> in response to error: T cannot be resolved to a type
     public static <T> ArrayList<Integer> binarySearchAll(T[] arr, T target, int N, Comparator<T> c) {
 
     }
+
+    
+
 }
